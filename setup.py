9534e1804c1dd639b4e53b7efd021fe80d257290
Julie Iskander <34782134+jIskCoder@users.noreply.github.com>
2025-03-04 19:59:20 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires=['py3Dmol','absl-py','biopython',
                       'chex','dm-haiku','dm-tree',
                       'immutabledict','jax','ml-collections',
-                      'numpy','pandas','scipy','optax','joblib',
+                      'numpy<2','pandas','scipy','optax','joblib',
                       'matplotlib'],
     include_package_data=True
 )
